a17d2f8ae25c106a0b631824c3907dadc4c6f4e3
Lot Kyqyku <lotkyqyku@gmail.com>
2022-11-04 11:49:05 +0100

    Welcome Layout

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,11 +1,7 @@
 <script>
 import { defineComponent, ref } from 'vue';
-import { Inertia } from '@inertiajs/inertia';
-import { Head, Link } from '@inertiajs/inertia-vue3';
-import Banner from '@/Components/Banner.vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+import  Nav  from '@/Components/Nav.vue'
 
 export default defineComponent({
     props:{
@@ -13,193 +9,18 @@ export default defineComponent({
     },
     components:{
         Head,
-        Link,
-        Dropdown,
-        DropdownLink,
-        ResponsiveNavLink,
+        Nav,
     }
 });
-
-const showingNavigationDropdown = ref(false);
-
-const logout = () => {
-    Inertia.post(route('logout'));
-};
 </script>
 
 <template>
     <div>
         <Head :title="title" />
-
-        <Banner />
-
         <div class="min-h-screen bg-gray-100">
-            <nav class="bg-white border-b border-gray-100">
-                <!-- Primary Navigation Menu -->
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="flex justify-between h-16">
-                        <div class="flex">
-                            <!-- Logo -->
-                            <div class="shrink-0 flex items-center">
-                                <Link :href="route('welcome')">
-                                        E-Need
-                                </Link>
-                            </div>
-
-                           
-                        </div>
-
-                        <div class="hidden sm:flex sm:items-center sm:ml-6">
-                            <div class="ml-3 relative">                          
-                            </div>
-
-                            <!-- Settings Dropdown -->
-                            <div class="ml-3 relative">
-                                <div class="flex items-center space-x-3 relative">
-                                    <Link :href="route('dashboard')"
-                                    class="hover:text-yellow-500
-                                    transition" v-if="$page.props.user">
-                                        Dashboard
-                                    </Link>
-                                    <template v-else>
-                                        <Link :href="route('register')"
-                                    class="hover:text-yellow-500
-                                    transition">
-                                        Register
-                                    </Link>
-                                        
-                                    <Link :href="route('login')"
-                                    class="hover:text-yellow-500
-                                    transition">
-                                        Login
-                                    </Link>
-
-                                    </template>
-                                    <Link href="#"
-                                    class="hover:text-yellow-500
-                                    transition">
-                                        Shop
-                                    </Link>
-                                    <form method="POST" @submit.
-                                    prevent="logout" v-if="$page.props.user">
-                                    <button type="submit"
-                                    class="hover:text-yellow-500
-                                    transition">
-                                    Log Out
-                                </button>
-                                    </form>
-                                    <Link href="#"
-                                    class="hover:text-red-700
-                                    transition">
-                                        <span class="bg-red-600 text-white text xs rounded-md p-1 absolute"
-                                        style="top: -15px; right: -12px;">
-                                            3
-                                        </span>
-                                        <svg aria-hidden="true"
-                                        data-prefix="fas"
-                                        data-icon="shoping-cart"
-                                        class="w-4 h-4 fill-current svg-inline--fa fa-shoping-cart fa-w-18" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"/>
-                                         </svg>
-
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Hamburger -->
-                        <div class="-mr-2 flex items-center sm:hidden">
-                            <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition" @click="showingNavigationDropdown = ! showingNavigationDropdown">
-                                <svg
-                                    class="h-6 w-6"
-                                    stroke="currentColor"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path
-                                        :class="{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                    <path
-                                        :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Responsive Navigation Menu -->
-                <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden">
-                    <template v-if="$page.props.user">
-                        <div class="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                            Dashboard
-                        </ResponsiveNavLink>
-                    </div>
-                    </template>
-                    <template v-else>
-                        <div class="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink :href="route('register')">
-                            Register
-                        </ResponsiveNavLink>
-                    </div>
-                    <div class="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink :href="route('login')">
-                            Login
-                        </ResponsiveNavLink>
-                    </div>
-                    </template>
-                    <div class="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink href="#">
-                            Shop
-                        </ResponsiveNavLink>
-                    </div>
-                    <div class="pt-2 pb-3 space-y-1">
-                       <Link href="#" class="flex items-center pl-3 pr-4
-                       py-2 border-1-4 border-transparent text-base
-                       font-medium text-gray-600 hover:text-red-700
-                       hover:border-red-700 focus:outline-none
-                       focus:text-gray-800 focus:bg-gray-50
-                       focus:border-gray-300 transition">
-
-                            <svg aria-hidden="true"
-                            data-prefix="fas"
-                            data-icon="shoping-cart"
-                            class="w-4 h-4 text-red-700 fill-current svg-inline--fa fa-shoping-cart fa-w-18" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"/>
-                            </svg>
-                            <span class="ml-2">
-                                3 item(s) in cart
-                            </span>
-                    </Link>
-                    </div>
-                 
-
-                    <!-- Responsive Settings Options -->
-                    <div class="pt-4 pb-1 border-t border-gray-200"
-                    v-if="$page.props.user">
-                        <div class="mt-3 space-y-1">                       
-                            <!-- Authentication -->
-                            <form method="POST" @submit.prevent="logout">
-                                <ResponsiveNavLink as="button">
-                                    Log Out
-                                </ResponsiveNavLink>
-                            </form>                  
-                        </div>
-                    </div>
-                </div>
-            </nav>
-
+           <Nav />
             <!-- Page Heading -->
-            <header v-if="$slots.header" class="bg-gray-700 shadow">
+            <header class="bg-gray-700 shadow">
                 <div class="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:px-8">
                     <h2 class="text-white text-center leading-tight">  
                         Lorem ipsum dolor sit amet.   
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,78 @@
 <template>
-    <app-layout title="Welcome">
-
+ <app-layout title="Welcome">
+        <div class="relative">
+            <img :src="'/storage/images/defaults/site_images/hand_craft.jpg'" alt="E-Need" class="w-full h-96 opacity-75 object-cover">
+            <div class="flex flex-col justify-around items-center absolute inset-0">
+                <span class="text-white text-xl">Lorem ipsum dolor sit amet.</span>
+                <div class="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
+                   <Link href="#" class="text-md justify-center inline-flex
+                    items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md
+                    font-semibold text-white uppercase tracking-widest hover:bg-gray-700
+                    active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring
+                    focus:ring-gray-300 transition">Womens</Link>
+                    <Link href="#" class="text-md justify-center inline-flex
+                    items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md
+                    font-semibold text-white uppercase tracking-widest hover:bg-gray-700
+                    active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring
+                    focus:ring-gray-300 transition">Mens</Link>
+                    <Link href="#" class="text-md justify-center inline-flex
+                    items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md
+                    font-semibold text-white uppercase tracking-widest hover:bg-gray-700
+                    active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring
+                    focus:ring-gray-300 transition">Kids</Link>
+                    <Link href="#" class="text-md justify-center inline-flex
+                    items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md
+                    font-semibold text-white uppercase tracking-widest hover:bg-gray-700
+                    active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring
+                    focus:ring-gray-300 transition">Home Goods</Link>
+                </div>
+            </div>
+        </div>
+        <div class="text-center border-t border-b py-4">
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div class="flex flex-col max-w-7xl mx-auto px-4 sm:container sm:flex-row sm:space-x-4 sm:my-4 sm:px-6 lg:px-8">
+            <Link href="#" class="flex flex-col w-full h-full mb-4">
+                <div class="text-center text-white bg-gray-700 py-2">
+                    <span>
+                        asdasfasf
+                    </span>
+                </div>
+                <img :src="'/storage/images/defaults/no_image.jpg'" alt="#" class="h-72 object-cover md:w-72 lg:w-96">
+            </Link>
+            <Link href="#" class="flex flex-col w-full h-full mb-4">
+                <div class="text-center text-white bg-gray-700 py-2">
+                    <span>
+                        asdasfasf
+                    </span>
+                </div>
+                <img :src="'/storage/images/defaults/no_image.jpg'" alt="#" class="h-72 object-cover md:w-72 lg:w-96">
+            </Link>
+            <Link href="#" class="flex flex-col w-full h-full mb-4">
+                <div class="text-center text-white bg-gray-700 py-2">
+                    <span>
+                        asdasfasf
+                    </span>
+                </div>
+                <img :src="'/storage/images/defaults/no_image.jpg'" alt="#" class="h-72 object-cover md:w-72 lg:w-96">
+            </Link>
+            <Link href="#" class="flex flex-col w-full h-full mb-4">
+                <div class="text-center text-white bg-gray-700 py-2">
+                    <span>
+                        asdasfasf
+                    </span>
+                </div>
+                <img :src="'/storage/images/defaults/no_image.jpg'" alt="#" class="h-72 object-cover md:w-72 lg:w-96">
+            </Link>
+            
+        </div>
+        <div class="text-center text-white bg-black px-4 py-4">
+            <p class="container">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto fuga neque culpa illum sit exercitationem enim dolores dolor quaerat quos, ex ipsa, recusandae in ad voluptates officia voluptatem, nobis animi!
+                Magni harum quos voluptas ex eveniet inventore rerum ducimus repellendus! Saepe, nam. Tempora optio ratione ipsa. Commodi est voluptatem excepturi dignissimos voluptas nostrum ut atque, repellendus quidem nesciunt, maiores fugiat?
+                Esse eligendi amet quo error, nostrum necessitatibus laudantium nulla rem quidem accusantium tenetur, aperiam qui excepturi! Natus, praesentium repellat esse optio fugiat id vel voluptatum deserunt minus, provident cum doloribus.
+            </p>
+        </div>
     </app-layout>
 </template>
 
@@ -8,6 +80,8 @@
 import { defineComponent} from 'vue';
 import { Link } from '@inertiajs/inertia-vue3';
 import AppLayout from '../Layouts/AppLayout.vue'
+
+
 
 
 export default defineComponent({
