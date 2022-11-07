fa860c9403903af91e7eb56c97af94a947e422e9
Lot Kyqyku <lotkyqyku@gmail.com>
2022-11-07 13:48:24 +0100

    Display Products

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -15,8 +16,17 @@ class ShopController extends Controller
      */
     public function index()
     {
+        $categories = Category::all();
+        if(request()->category){
+            $products = Product::whereHas('categories', function($query){
+                $query->where('slug',request()->category);
+            })->inRandomOrder()->get();
+        } else {
+            $products = Product::inRandomOrder()->get();
+        }
         return Inertia::render('Shop/Index',[
-            'products' => Product::all(),
+            'products' => $products,
+            'categories' => $categories,
         ]);
     }
 
--- resources/js/Pages/Shop/Index.vue
+++ resources/js/Pages/Shop/Index.vue
@@ -1,11 +1,58 @@
 <template>
-    {{ products }}
-</template>
+    <app-layout>
+        <header class="text-gray-7-- bg-gray-300 shadow">
+            <div class="flex justify-between items-center max-w-7xl
+            mx-auto py-2 px-4 sm:px-6 lg:px-8">
+            <div class="flex items-center leading-tight">
+                <Link :href="route('welcome')" class="text-gray-700
+                transition hover:text-yellow-500">Home</Link>
+            </div>
+            <div class="w-1/2">
+            </div>
+        </div>
+        </header>
+        <div class="flex">
+            <div class="border-r w-1/5">
+                <div>
+                    <div class="text-white text-center bg-gray-700 py-4">
+                        <p>Shop By Category</p>
+                    </div>
+                    <div class="flex flex-col divide-y">
+                        <Link :href="route('shop.index', { category: category.slug })" class="text-left px-4 py-4 transition hover:text-white hover:bg-gray-700 sm:px-6 lg:px-8" :class="route().current('shop.index', { category: category.slug}) ? 'bg-gray-700 text-white' : ''" v-for="(category, index) in categories" :key="index">
+                            {{category.name}}
+                        </Link>
+                    </div>
+                </div>
+            </div>
 
+            <div class="border-l w-4/5">
+                <div class="container flex flex-wrap mx-auto">
+                    <Link href="#" class="flex flex-col w-full p-4 rounded sm:w-1/2 md:w-1/3" v-for="(product, index) in products" :key="index">
+                        <img :src="'/storage/products/'+product.image" :alt="product.name" class="h-72 object-cover md:w-72 lg:w-96">
+                        <div class="flex justify-around bg-gray-700 py-2">
+                            <span class="text-yellow-500">{{product.price }}</span>
+                            <span class="text-white">{{ product.name }}</span>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+  
 
 <script>
     import { defineComponent } from 'vue'
+    import AppLayout from '@/Layouts/AppLayout.vue'
+    import { Link } from '@inertiajs/inertia-vue3';
+
+
     export default defineComponent({
-        props: ['products'],
+        props: ['products','categories'],
+        components: {
+        AppLayout,
+         Link,
+
+},
     })
 </script>
